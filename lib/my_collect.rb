f3eda44b34c3d5cad058a10fc6b47956f22c42b0
Christian Fredericksen <christian.fredericksen@yahoo.com>
2019-12-21 02:21:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+  i = 0 
+  capital_collection =[]
+  while i < collection.length
+    capital_collection << yield(collection[i])
+    i += 1 
+  end
+  capital_collection
+end
 
 
